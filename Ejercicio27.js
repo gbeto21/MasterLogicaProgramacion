93c6ca552b5b41487dd234e8179d9dc2297ba8cc
Alberto González Balderas <albertoglezb@hotmail.com>
2022-01-24 19:59:14 -0600

    El ejercicio 27 fue resuelto.

---
+++ Ejercicio27.js
@@ -0,0 +1,13 @@
+/*
+Dado un numero mostrar a cuantos años, dias y meses equivale
+*/
+
+function calcularDias(dias) {
+  let anios = Math.floor(dias / 365);
+  let dias_restantes = dias % 365;
+  let meses = Math.floor(dias_restantes / 30);
+  dias_restantes = dias_restantes % 30;
+  return `Equivale a ${anios} años, ${meses} meses y ${dias_restantes} días.`;
+}
+
+console.log(calcularDias(789));
