1dcf2cd75cef28991100be5756d4b732aa8c514c
Alberto González Balderas <albertoglezb@hotmail.com>
2022-01-25 20:15:51 -0600

    El ejercicio 42 fue resuelto.

---
+++ Ejercicio42.js
@@ -0,0 +1,9 @@
+/*
+Dados dos numeros sacar un número aleatorio entre ellos
+*/
+
+function aleatorio(minimo, maximo) {
+  return Math.round(Math.random() * (maximo - minimo) + minimo);
+}
+
+console.log(aleatorio(1, 40));
